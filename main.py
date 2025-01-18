f7e82da3efa59d148b6011b87d2dcccb12392352
Maksim Ushakov <dmeaaxd@gmail.com>
2025-01-18 15:29:08 +0300

    ref main.py

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import uvicorn
 
 if __name__ == '__main__':
-    uvicorn.run("app.fastapi_app:app", host="localhost", port=8910, reload=True)
+    uvicorn.run("app.fastapi_app:app", host="0.0.0.0", port=8910, reload=True)
 
